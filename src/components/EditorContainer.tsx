ab623b5cb748c0b2cffb7254e0ef23760882525f
Colby Sieber <sieber.colby@gmail.com>
2018-05-16 19:26:18 -0700

    Added background layer container and basic png save functionality

--- src/components/EditorContainer.tsx
+++ src/components/EditorContainer.tsx
@@ -12,8 +12,26 @@ const containerStyles: React.CSSProperties = {
 	flexDirection: 'column',
 	alignItems: 'center',
 	justifyContent: 'center',
+	position: 'relative',
+}
+
+const innerContainerStyles: React.CSSProperties = {
 	padding: '60px 100px 100px 60px',
+}
+
+const alphaImage: React.CSSProperties = {
+	width: '100%',
+	height: '100%',
 	background: 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAMUlEQVQ4T2NkYGAQYcAP3uCTZhw1gGGYhAGBZIA/nYDCgBDAm9BGDWAAJyRCgLaBCAAgXwixzAS0pgAAAABJRU5ErkJggg==)',
+};
+
+const backgroundLayers: React.CSSProperties = {
+	zIndex: -1,
+	position: 'absolute',
+	top: 0,
+	bottom: 0,
+	left: 0,
+	right: 0,
 }
 
 export default class EditorContainer extends React.Component<EditorContainerProps> {
@@ -21,8 +39,13 @@ export default class EditorContainer extends React.Component<EditorContainerProp
 		const { config } = this.props;
 
 		return (
-			<div style={containerStyles}>
-				<MonacoEditor config={config} />
+			<div style={containerStyles} id="container">
+				<div style={innerContainerStyles}>
+					<MonacoEditor config={config} />
+					<div style={backgroundLayers}>
+						<div style={alphaImage} className="eliminateOnSave" />
+					</div>
+				</div>
 			</div>
 		);
 	}
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { EditorConfig } from '../types';
 
+import domToImage, { Options } from 'dom-to-image';
+
 export interface ToolbarProps {
 	config: EditorConfig;
 	onChange: (config: EditorConfig) => any;
@@ -14,9 +16,40 @@ export default class Toolbar extends React.Component<ToolbarProps> {
 		} = this.props;
 
 		return <div>
-			
+			<button onClick={this.saveImage}>Save Image</button>
 		</div>
 	}
+
+	private takeImage = () => {
+		const node = document.getElementById('container');
+
+		if (!node) {
+			throw new Error('Could not find the editor container');
+		}
+
+		const config: Options = {
+			filter: (n: any) => {
+				if (n.className) {
+					return String(n.className).indexOf('eliminateOnSave') < 0;
+				}
+
+				return true;
+			}
+		};
+
+		return domToImage.toPng(node, config);
+	}
+
+	private saveImage = () => {
+		const link = document.createElement('a')
+		this.takeImage().then(url => {
+				link.download = `carbon.png`;
+				link.href = url;
+				document.body.appendChild(link);
+				link.click();
+				link.remove();
+			});
+		};
 
 	private changeValue<K extends keyof EditorConfig>(key: K, value: EditorConfig[K]) {
 		const { onChange, config } = this.props;
